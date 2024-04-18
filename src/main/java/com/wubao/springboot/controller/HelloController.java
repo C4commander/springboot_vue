912a62d33ef9609d23600d2145ee547b46bb677d
C4commander <770505240@qq.com>
2024-04-18 22:39:36 +0800

    集成热部署

--- src/main/java/com/wubao/springboot/controller/HelloController.java
+++ src/main/java/com/wubao/springboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.wubao.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
+    @Value("${test.hello:This is default}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String say_hello(){
-        return "Hello World!";
+        return "Hello World!"+testHello;
     }
 
     @RequestMapping("/hello/post")
